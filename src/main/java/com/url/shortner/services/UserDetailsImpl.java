c4e00f25964cc33cef68da52e0bd902e5d2ca862
Ashutosh <74343946+ashusharma07@users.noreply.github.com>
2025-01-26 21:06:26 +0530

    First Commit.

---
+++ src/main/java/com/url/shortner/services/UserDetailsImpl.java
@@ -0,0 +1,42 @@
+package com.url.shortner.services;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
